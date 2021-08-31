bca51a785ebbbf59318c12167694495205786e1d
Mohamed Rafeek <56187448+rafeekbinhussain@users.noreply.github.com>
2021-08-31 23:27:43 +0530

    Added test and README.md

--- src/main/java/com/preference/userprofile/FetchUserApplication.java
+++ src/main/java/com/preference/userprofile/FetchUserApplication.java
@@ -1,15 +1,18 @@
 package com.preference.userprofile;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@Slf4j
 public class FetchUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FetchUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FetchUserApplication.class, args);
+        log.info("Welcome to TB Pay Fetch user Preferences Service");
+    }
 
 }
